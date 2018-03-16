eb824cb89a0041a5b4ccdb743d54f00d03425bb5
fireflightg <35312792+fireflightg@users.noreply.github.com>
2018-03-16 14:18:22 -0400

    yah change to sci project

---
+++ sci.c
@@ -0,0 +1,39 @@
+﻿//*!!Sensor,    dgt16,          Bumbswitch, BumbswitchActive,      ,              !!*//
+//*!! Motor,     motor1,         Motorvalue, MotorActive,                                                                !!*//
+//*!!Start automatically generated configuration code.                           !!*//
+const tSensors Bumbsensor          = dgt16;
+const tMotor  Motorsensor          = Motor393;
+//sensorLightActive  //*!!!!*//
+//*!!CLICK to edit 'wizard' created sensor & motor configuration.                !!*//
+
+
+// This program uses the light sensor to track the left side of a line
+
+task main ()
+
+{
+
+if (untilTouch(dgtl6);)
+
+{
+    
+startmotor(Motor393);
+        
+ 
+
+forward(23);
+     until
+
+
+// do something
+
+}
+else if (sensorValue[0]){
+
+forward(0);
+}
+ else () {
+  forward(0);       
+ } 
+stop(); 
+}
